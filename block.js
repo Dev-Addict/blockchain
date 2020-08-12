d700344569c83311703c8bc54c1ba0fd1ba52242
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-13 02:25:18 +0430

    create mineBlock and hashing function

--- block.js
+++ block.js
@@ -1,8 +1,20 @@
 const { GENESIS_DATA } = require("./config");
+const cryptoHash = require("./crypto-hash");
 
 class Block {
   static genesis() {
     return new this(GENESIS_DATA);
+  }
+
+  static mineBlock({ lastBlock: { hash }, data }) {
+    const timestamp = Date.now();
+
+    return new this({
+      timestamp,
+      lastHash: hash,
+      hash: cryptoHash(timestamp, hash, data),
+      data,
+    });
   }
 
   constructor({ timestamp, lastHash, hash, data }) {
