f431ebcb5a8ba5090f3aa96a4f6b02a33d2b68f1
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-18 13:11:22 +0430

    update pusub and create different ports

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config({
   path: path.join(__dirname, "./config.env"),
 });
 
+const net = require("net");
 const express = require("express");
 const request = require("request");
 
@@ -12,7 +13,7 @@ const Blockchain = require("./blockchain");
 const PubSub = require("./pubsub");
 const CHANNELS = require("./channels");
 
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 
 const ROOT_NODE_ADDRESS = `http://127.0.0.1:${PORT}`;
 
@@ -70,8 +71,32 @@ const syncChains = () => {
   });
 };
 
-app.listen(PORT, () => {
-  console.log(`|> Ready on port ${PORT}.`);
+const isPortFree = (port) =>
+  new Promise((resolve) => {
+    const server = require("http")
+      .createServer()
+      .listen(port, () => {
+        server.close();
+        resolve(true);
+      })
+      .on("error", () => {
+        resolve(false);
+      });
+  });
 
-  syncChains();
-});
+const findPort = async (port) => {
+  if (await isPortFree(port)) return port;
+  return await findPort(parseInt(port) + 1);
+};
+
+findPort(PORT)
+  .then((port) => {
+    PORT = port;
+
+    app.listen(PORT, () => {
+      console.log(`|> Ready on port ${PORT}.`);
+
+      if (PORT !== process.env.PORT || 3000) syncChains();
+    });
+  })
+  .catch((err) => console.log(err));
